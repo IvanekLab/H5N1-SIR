81dc9ff4a5e47942daf4f762ec2558b56dd227ed
KatKoebelDVM <143016995+KatKoebelDVM@users.noreply.github.com>
2026-07-14 13:19:59 -0400

    Updating model.R to latest documentation
    This branch's model.R is now up-to-date with the documentation in ODE-v7b

--- model.R
+++ model.R
@@ -25,19 +25,19 @@ HPAI_dyn <- function(time, state, parameters) {
       kappa_4 * kappa_2 * beta_c * H_D
     
     dS_L <- B + gamma * R_L + f * S_D - lambda_L * S_L - (d + mu1 + mu2) * S_L
-    dI_Lc <- f * I_Dc + p * lambda_L * S_L - (d + mu1 + mu2 + alpha_c + ro * sigma + delta_1 + delta_2) * I_Lc
+    dI_Lc <- f * I_Dc + p * (1-ro) * lambda_L * S_L - (d + mu1 + mu2 + alpha_c + delta_1 + delta_2) * I_Lc
     dI_Ls <- f * I_Ds + (1 - p) * lambda_L * S_L - (d + mu1 + mu2 + alpha_s) * I_Ls
-    dH_L <- f * H_D + ro * sigma * I_Lc - (d + mu1 + mu2 + alpha_H + delta_1 + delta_2) * H_L
-    dR_L <- f * R_D + alpha_s * I_Ls + alpha_c * I_Lc + alpha_H * H_L - (d + mu1 + mu2 + gamma) * R_L
-    dR_Lc <- f * R_Dc + alpha_c * I_Lc + alpha_H * H_L - (d + mu1 + mu2 + gamma) * R_Lc
+    dH_L <- f * H_D + p * ro * lambda_L * S_L - (d + mu1 + mu2 + alpha_H + delta_1 + delta_2) * H_L
+    dR_L <- f * R_D + alpha_s * I_Ls + alpha_c * I_Lc - (d + mu1 + mu2 + gamma) * R_L
+    dR_Lc <- f * R_Dc + alpha_c * I_Lc - (d + mu1 + mu2 + gamma) * R_Lc
     dR_Ls <- f * R_Ds + alpha_s * I_Ls - (d + mu1 + mu2 + gamma) * R_Ls
     
     dS_D <- gamma * R_D + d * S_L - lambda_D * S_D - (f + mu1 + mu2) * S_D
-    dI_Dc <- d * I_Lc + p * lambda_D * S_D - (f + mu1 + mu2 + alpha_c + ro * sigma + delta_1 + delta_2) * I_Dc
+    dI_Dc <- d * I_Lc + p * (1-ro) * lambda_D * S_D + alpha_H * H_D - (f + mu1 + mu2 + alpha_c + delta_1 + delta_2) * I_Dc
     dI_Ds <- d * I_Ls + (1 - p) * lambda_D * S_D - (f + mu1 + mu2 + alpha_s) * I_Ds
-    dH_D <- d * H_L + ro * sigma * I_Dc - (f + mu1 + mu2 + alpha_H + delta_1 + delta_2) * H_D
-    dR_D <- d * R_L + alpha_s * I_Ds + alpha_c * I_Dc + alpha_H * H_D - (f + mu1 + mu2 + gamma) * R_D
-    dR_Dc <- d * R_Lc + alpha_c * I_Dc + alpha_H * H_D - (f + mu1 + mu2 + gamma) * R_Dc
+    dH_D <- d * H_L + ro * p * lambda_D * S_D - (f + mu1 + mu2 + alpha_H + delta_1 + delta_2) * H_D
+    dR_D <- d * R_L + alpha_s * I_Ds + alpha_c * I_Dc - (f + mu1 + mu2 + gamma) * R_D
+    dR_Dc <- d * R_Lc + alpha_c * I_Dc - (f + mu1 + mu2 + gamma) * R_Dc
     dR_Ds <- d * R_Ls + alpha_s * I_Ds - (f + mu1 + mu2 + gamma) * R_Ds
     
     dS_W <- (1 - v) * B_W - lambda_W * S_W - z * S_W
